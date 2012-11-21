d0339cc2152092fcb2a5011219bf593bb4e14ae5
lastid <lastid@lastid>
2012-11-21 20:58:00 +0100

    Change prox free url from https to http

--- js/config.js
+++ js/config.js
@@ -43,7 +43,7 @@ var Config = {
 	PROXIES: {
 		proxfree: {
 			method: 'POST',
-			action: 'https://eu.proxfree.com/request.php?do=go',
+			action: 'http://eu.proxfree.com/request.php?do=go',
 			urlField: 'get',
 			defaultParams: {}
 		}
